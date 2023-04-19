d71c4efba5c1088302ffbdfa0d743c7300c36a52
Víctor López <vicrenlopez14115@gmail.com>
2023-04-19 15:30:49 -0600

    Project reboot
    Android XML View's now the base of the project.

---
+++ app/src/main/java/com/example/camvi/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.camvi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.ui.Modifier
+import com.example.camvi.ui.theme.CamviTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CamviTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column {
+                        Text("Si funciona")
+                        Button(onClick = { /*TODO*/ }) {
+                            Text("Hola")
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
