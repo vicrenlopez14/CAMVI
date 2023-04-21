a6b8bd3129db186e181ed9061e0c827c63de3353
Víctor López <vicrenlopez14115@gmail.com>
2023-04-21 12:13:56 -0600

    Navigation with Compose

--- app/src/main/java/com/example/camvi/ui/screens/administradores/AdministradoresDashboardScreen.kt
+++ app/src/main/java/com/example/camvi/ui/screens/administradores/AdministradoresDashboardScreen.kt
@@ -1,11 +1,23 @@
-package com.example.camvi.ui.screens.administradores
+package com.example.camvi.ui.widgets.administradores
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.camvi.ui.widgets.global.DrawerNavGraph
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AdministradoresDashboardScreen() {
+fun AdministradoresMainScreen() {
     val navController = rememberNavController()
 
+    Scaffold() {
+        Box(modifier = Modifier.padding(it)) {
+            DrawerNavGraph(navController = navController)
+        }
+    }
 
 }
