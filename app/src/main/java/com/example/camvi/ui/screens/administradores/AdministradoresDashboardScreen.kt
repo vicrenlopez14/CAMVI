737fb755827c051fbec57044cfb7ede35ef9a9ec
Víctor López <vicrenlopez14115@gmail.com>
2023-04-21 11:19:11 -0600

    AGP Version problem fix
    By Susan

---
+++ app/src/main/java/com/example/camvi/ui/screens/administradores/AdministradoresDashboardScreen.kt
@@ -0,0 +1,11 @@
+package com.example.camvi.ui.screens.administradores
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DashboardScreen() {
+    val navController = rememberNavController()
+
+
+}
