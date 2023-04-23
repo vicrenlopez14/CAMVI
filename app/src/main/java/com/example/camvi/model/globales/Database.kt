0b44153de2b3a6edefd60508c7a65af891affffc
Víctor López <vicrenlopez14115@gmail.com>
2023-04-23 10:36:54 -0600

    Database connection

---
+++ app/src/main/java/com/example/camvi/model/globales/Database.kt
@@ -0,0 +1,13 @@
+package com.example.camvi.model.globales
+
+import org.ktorm.database.Database
+
+object Database {
+    val database = Database.connect(
+        "jdbc:sqlserver://localhost:1433;databaseName=CamviDatabase",
+        driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+        user = "sa",
+        password = "Rical2023*"
+    )
+
+}
